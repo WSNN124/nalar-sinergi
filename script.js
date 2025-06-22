cfd4b79d213d77680d8ddb2b1cf5239091618a4b
unknown <sonwin61@gmail.com>
2025-06-22 16:32:54 +0700

    tambah banner, perbaiki bbrp

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const floatingInput = document.querySelector("#floatingInput");
 const floatingPassword = document.querySelector("#floatingPassword");
 const loginButton = document.querySelector("#login-btn");
+const signUpButton = document.querySelector("#signUp-btn");
 
-loginButton.addEventListener("click", function () {
-  if (
-    floatingInput.value == "admin123@gmail.com" &&
-    floatingPassword.value == "admin123"
-  ) {
-    window.location.href = "index.html";
-  } else {
-    alert("password salah");
-  }
-});
+if (signUpButton) {
+  signUpButton.addEventListener("click", function () {
+    if (floatingInput.value != "" && floatingPassword.value != "") {
+      window.location.href = "index.html";
+    }
+  });
+}
+
+if (loginButton) {
+  loginButton.addEventListener("click", function () {
+    if (
+      floatingInput.value == "admin123@gmail.com" &&
+      floatingPassword.value == "admin123"
+    ) {
+      window.location.href = "index.html";
+    } else {
+      alert("password salah");
+    }
+  });
+}
